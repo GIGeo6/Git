2694ae900ce8ee715d1bb93cf1da9f3253c2cc8c
GIGeo6 <44166462+GIGeo6@users.noreply.github.com>
2018-10-31 16:13:35 +0100

    index

---
+++ GroupeFinder/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    <title>Groups</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello Boys</p>"
+        ?>
+        <a href="login.php"> Click here to log in
+        <a href="register.php"> Click her to register
+    </body>
+</html>
---
+++ GroupeFinder/register.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Groups</title>
+    </head>
+    <body>
+        <h2>Registration</h2>
+        <a href="index.php">Back<br/><br/>
+        <form action="register.php" method="POST">
+            Enter email: <input type="text" name="email" required="required" /> <br/>
+            Enter password: <input type="password" name="password" required="required" /> <br/>
+            <input type="submit" value="Register" />
+        </form>
+    </body>
+</html>
