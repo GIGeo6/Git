b636bf36f5bcdd218970df79b765637884ea3be3
GIGeo6 <44166462+GIGeo6@users.noreply.github.com>
2018-11-01 15:14:25 +0100

    index/registration

--- index.php
+++ index.php
@@ -7,7 +7,12 @@
         <?php
             echo "<p>Hello Boys</p>";
         ?>
-        <a href="login.php"> Click here to log in </a><br/>
-        <a href="register.php"> Click her to register</a>
+        <form action="checklogin.php" method="POST">
+            Enter email: <input type="text" name="email" required="required" /> <br/>
+            Enter password: <input type="password" name="password" required="required" /> <br/>
+            <input type="submit" value="Login" />
+        </form>
+        <br/><br/>
+        <a href="register.php"> Not member yet ? Click her to register</a>
     </body>
 </html>
--- register.php
+++ register.php
@@ -7,8 +7,9 @@
         <h2>Registration</h2>
         <a href="index.php">Back</a><br/><br/>
         <form action="register.php" method="POST">
-            Enter Email: <input type="text" name="email" required="required" /> <br/>
-            Enter Password: <input type="password" name="password" required="required" /> <br/>
+            Email: <input type="text" name="email" required="required" /> <br/>
+            Password: <input type="password" name="password" required="required" /> <br/>
+            Birth date: Day<input type="text" name="bday" required="required"/> Month<input type="text" name="bmonth" required="required" /> Year<input type="text" name="byear" required="required" />
             <input type="submit" value="Register" />
         </form>
     </body>
